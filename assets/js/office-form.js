b5c6fed98366e3deb3f0675c1c703670eaa9bc1a
Russellg <103833866+RussellGN@users.noreply.github.com>
2022-11-16 14:58:05 +0200

    reduced monetisation options, directory prices and inc office prices

--- assets/js/office-form.js
+++ assets/js/office-form.js
@@ -1,36 +1,10 @@
-const nextBtns = document.querySelectorAll(".office-form-next");
-const prevBtns = document.querySelectorAll(".office-form-prev");
 const optionsType = document.querySelector(".options-type");
 const optionsList = document.querySelector(".options-list");
 const optionsPrice = document.querySelector(".options-price");
-const numberOfTabs = 6;
-
-nextBtns.forEach((btn) => {
-	btn.addEventListener("click", (e) => {
-		const nextTab =
-			Number(
-				e.target.parentElement.parentElement.id.charAt(
-					e.target.parentElement.parentElement.id.length - 1
-				)
-			) + 1;
-		document.querySelector(`#office-tabs #toggle${nextTab}`).click();
-	});
-});
-
-prevBtns.forEach((btn) => {
-	btn.addEventListener("click", (e) => {
-		const prevTab =
-			Number(
-				e.target.parentElement.parentElement.id.charAt(
-					e.target.parentElement.parentElement.id.length - 1
-				)
-			) - 1;
-		document.querySelector(`#office-tabs #toggle${prevTab}`).click();
-	});
-});
 
 document.addEventListener("DOMContentLoaded", (e) => {
 	officeDetails = JSON.parse(localStorage.getItem("officeDetails"));
+	console.log(officeDetails);
 
 	let items = "";
 
@@ -44,13 +18,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
 		items += container;
 	});
 
-	("Business Research Bureau");
+	("Research Bureau Newsletter");
 	("Business Consultancy");
 	optionsList.innerHTML = items;
 	optionsType.innerHTML = officeDetails.DD
 		? `You selected Due Dilligence(All) under <br/> ${officeDetails.block.replace("iNc", "")}`
 		: `You selected the following under <br/> ${officeDetails.block.replace("iNc", "")} :`;
-	optionsPrice.textContent = officeDetails.DD ? "$50" : "$29.99";
-
-	console.log(officeDetails, optionsList, optionsType);
+	optionsPrice.textContent = officeDetails.DD ? "£35" : "£29.90";
 });
